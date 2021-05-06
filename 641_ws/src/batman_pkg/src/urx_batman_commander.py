4b5b66cd362994f09130acdc01236e44186ed012
Wafts <Wafic.El-Ariss@bmw.de>
2021-05-06 20:09:04 +0200

    preliminary work to make UR10 robot draw batman

---
+++ 641_ws/src/batman_pkg/src/urx_batman_commander.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import rospy
+from waypoints import get_ctrs
+import glob, os
+from os.path import dirname, abspath
+pkg_path = dirname(dirname(abspath(__file__)))
+# import urx
+
+def main():
+    rospy.init_node("turtlesim_batman_commander")
+    # rob = urx.Robot("")
+    # my_batman_turtle = TurtleBot()
+    media_path = os.path.join(pkg_path, "media")
+
+    print("Below are the available logos that can be drawn:\n")
+    for idx, file in enumerate(os.listdir(media_path)):
+        if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png"):
+            print("%i- %s" % (idx, file))
+
+    desired_logo_idx = int(input("\nSelect the number of the desired logo to draw using turtlesim: "))
+    desired_logo_path = os.path.join(media_path, os.listdir(media_path)[desired_logo_idx])
+    # print("desired_logo_path", desired_logo_path)
+    my_ctrs, my_shape = get_ctrs(desired_logo_path, False)
+    aspect_ratio = my_shape[0]/my_shape[1]
+    x_factor = 0.3 / my_shape[1]
+    y_factor = 0.3 * aspect_ratio / my_shape[0]
+    # print("type(my_ctrs)", type(my_ctrs))
+    waypoints = my_ctrs[0]
+    print("number of waypoints from the contour: ", len(waypoints))
+
+    goals = []
+    for idx, wp in enumerate(waypoints):
+        # my_goal = [0,0,0,0,0,0]
+        my_goal[0] = wp[0][0] * x_factor
+        my_goal[1] = (my_shape[0]-wp[0][1]) * y_factor
+        goals.append(my_goal)
+
+    # my_batman_turtle.move_waypoints(goals, 0.05)
+    # rob.movels()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except rospy.ROSInterruptException: pass
